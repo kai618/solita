8dfa297c5a601cd901c7de5ec760b1483ef3e9aa
kai618 <vanhieu1601@gmail.com>
2020-03-21 22:37:38 +0700

    fixed the name of the game

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Omnicard',
+      // Klondike Solitaire
+      title: 'Solita',
       theme: ThemeData(primarySwatch: Colors.green),
       home: PlayScreen(),
     );
